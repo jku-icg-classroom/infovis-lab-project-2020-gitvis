4d2def029b3343e628e81dd536cb9cebdc3d83c6
eliburgi <elias.burgstaller@gmx.at>
2020-05-31 15:40:06 +0200

    added logo to header

--- code/src/header/header.js
+++ code/src/header/header.js
@@ -1,36 +1,41 @@
 
 function createHeaderVis(parentDiv) {
+    let logo = parentDiv.append("img")
+    .attr("src", "img/logo.png")
+    .attr("height", 100)
+    .attr("width", "auto");
+
     let authorsDiv = parentDiv.append("div").attr("id", "authors");
-    let authorsLabel = authorsDiv.append("h4").attr("id", "authors_label").text("Authors");
-    let authorsRow = authorsDiv.append("div").attr("id", "authors_row");
+    authorsDiv.append("h4").attr("id", "authors_label").text("Authors");
+    authorsDiv.append("div").attr("id", "authors_row");
 }
 
 function updateHeaderVis(data) {
     // TODO: make this nicer xD
     d3.select('#authors_row').selectAll("div")
         .data(state.authors)
-        .style('background-image', author => 'url(' + author.avatar_url +')')
-        .style('opacity', function (author) { 
-            if(state.selectedAuthor && state.selectedAuthor.id === author.id) {
+        .style('background-image', author => 'url(' + author.avatar_url + ')')
+        .style('opacity', function (author) {
+            if (state.selectedAuthor && state.selectedAuthor.id === author.id) {
                 return 1.0;
             }
             return 0.5;
-         })
+        })
         .enter()
         .append("div")
         .attr('class', 'author_avatar')
-        .style('background-image', author => 'url(' + author.avatar_url +')')
-        .style('opacity', function (author) { 
-            if(state.selectedAuthor && state.selectedAuthor.id === author.id) {
+        .style('background-image', author => 'url(' + author.avatar_url + ')')
+        .style('opacity', function (author) {
+            if (state.selectedAuthor && state.selectedAuthor.id === author.id) {
                 return 1.0;
             }
             return 0.7;
-         })
+        })
         .on("click", function (author) {
-            if(state.selectedAuthor && state.selectedAuthor.id === author.id) {
+            if (state.selectedAuthor && state.selectedAuthor.id === author.id) {
                 deselectAuthor();
             } else {
                 selectAuthor(author);
-            }            
+            }
         });
 }
