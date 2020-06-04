7d37f7f37f128f112d91382e960883c685fba3bc
Richard Wolfmayr <richard.wolfmayr@gmail.com>
2020-06-04 10:38:32 +0200

    node rect color css fix

--- code/src/history/history.js
+++ code/src/history/history.js
@@ -240,7 +240,6 @@ function renderHistoryGraphFromTo() {
 
     createLinesChangedChart();      //#Mike
 
-
     debugger
     updateHistoryVis(state.filteredData);
     updateRepoDetails(state.filteredData);    //TODO check if this is the right position; Repo-Details just need to be updated if the repository changes
